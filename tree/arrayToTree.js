f3341a03998e2fb9fbe85ebc75f5a9d7bed47571
wuaixiaoyao <837865811@qq.com>
2020-03-04 10:46:26 +0800

    feat: 完善逻辑

--- tree/arrayToTree.js
+++ tree/arrayToTree.js
@@ -17,45 +17,82 @@ arrEG = [{
 }]
 
 
- //参考： https://blog.csdn.net/qq_33594380/article/details/82462701
+//参考： https://blog.csdn.net/qq_33594380/article/details/82462701
 
 /**
  * arrar e.g. 
  */
-var nodes = [
-  {id: 10, title: 'dw10', parentId: 4},
-  {id: 2, title: 'dw2', parentId: 0},
-  {id: 4, title: 'dw4', parentId: 2},
-  {id: 12, title: 'dw12', parentId: 2},
-  {id: 8, title: 'dw8', parentId: 4}
+var nodes = [{
+    id: 10,
+    title: 'dw10',
+    parentId: 4
+  },
+  {
+    id: 2,
+    title: 'dw2',
+    parentId: 0
+  },
+  {
+    id: 4,
+    title: 'dw4',
+    parentId: 2
+  },
+  {
+    id: 12,
+    title: 'dw12',
+    parentId: 2
+  },
+  {
+    id: 8,
+    title: 'dw8',
+    parentId: 4
+  }
 ];
- 
+
 // sort
 function cmp(a, b) {
   return a.parentId - b.parentId;
 }
 nodes.sort(cmp);
- 
+
 var midObj = {};
 // 从后向前遍历
-for(var i = nodes.length - 1; i >= 0; i--) {
+for (var i = nodes.length - 1; i >= 0; i--) {
   var nowPid = nodes[i].parentId;
   var nowId = nodes[i].id;
-  // 建立当前节点的父节点的children 数组
-  if(midObj[nowPid]) {
+  /**
+   * 建立当前节点的父节点的children 数组，
+   * e.g.: 
+   * { 22: [
+   *    {
+   *      id: 224,
+          title: 'dw4',
+           parentId: 22
+   *    },
+         {
+   *      id: 225,
+          title: 'dw4',
+           parentId: 22
+   *    }
+   * ] }
+   */
+  if (midObj[nowPid]) {
+    //存在父节点时， 直接push
     midObj[nowPid].push(nodes[i]);
   } else {
+    //不存在时 先创建数组 
     midObj[nowPid] = [];
     midObj[nowPid].push(nodes[i]);
   }
   // 将children 放入合适的位置
-  if(midObj[nowId]) {
+  if (midObj[nowId]) {
+    //若midobj 已经存在当前节点  把当前节点 作为childern 赋值给 nodes[i] 并删除 midobj[nowId]
     nodes[i].children = midObj[nowId];
     delete midObj[nowId];
   }
 }
 console.log('----------------')
- 
+
 console.log(midObj[0][0])
 
 console.log('----------------')
